4945dbfde31705ff4fedfc9ad00373ba2ae84c0f
Scientia <srsshashina@gmail.com>
2026-07-30 07:19:28 +0700

    latest change ++new tugas

--- template-trial/routes/web.php
+++ template-trial/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('muvid', function () {
+    return view('indexMuvid');
+});
+
+Route::get('nexus', function () {
+    return view('indexNexus');
+});
